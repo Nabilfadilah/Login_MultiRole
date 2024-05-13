453a05aad698903f7f98a42b6d0753c2abb175eb
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-13 21:50:19 +0700

    menambahkan dontentv

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,27 @@
 import express from "express"
 import cors from "cors"
 import session from "express-session"
+import dontenv from "dotenv"
+
+dontenv.config();
 
 const app = express();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}))
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+
+app.use(express.json());
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server up and running...')
