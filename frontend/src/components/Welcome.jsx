ad763de7dfa801ae62919e2954abd68fc33424be
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-16 09:32:47 +0700

    membuat conten di dashboard

---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1 className="title has-text-danger">Dashboard</h1>
+      <h2 className="subtitle">Welcome Back ...</h2>
+    </div>
+  );
+};
+
+export default Welcome;
