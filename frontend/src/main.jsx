eac94d4800a0156f316a90251ee52326df82effa
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-16 16:40:24 +0700

    commit cobain

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store.js";
 import "bulma/css/bulma.css";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 const root = createRoot(container);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <App />
     </Provider>
   </React.StrictMode>
