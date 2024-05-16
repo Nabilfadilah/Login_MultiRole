df8a0e4099157f197a37a134cf2ec26c9c950d37
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-16 09:33:58 +0700

    membuat tabel produk

---
+++ frontend/src/components/ProductList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1 className="title has-text-danger">Product</h1>
+      <h2 className="subtitle has-text-grey">List of Product</h2>
+      <table className="table is-striped is-fullwidth">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Created By</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductList;
