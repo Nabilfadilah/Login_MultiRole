f6776a9fd2be9fe66a5a6faecc803599aa5ffc01
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-17 16:53:40 +0700

    memberikan selector di dashboard untuk nama user yang login

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Welcome = () => {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <div>
       <h1 className="title has-text-danger">Dashboard</h1>
-      <h2 className="subtitle">Welcome Back ...</h2>
+      <h2 className="subtitle">
+        Welcome Back{" "}
+        <strong style={{ color: "black" }}>{user && user.name}</strong>
+      </h2>
     </div>
   );
 };
