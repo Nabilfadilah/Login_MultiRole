55ca614afcc01294f780432a0d916494437fd366
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-17 17:37:33 +0700

    integrasi getProduct, delete product, dan membuat terhubung dari button add dan edit

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  // method fecting data
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+    setProducts(response.data);
+    console.log(response.data);
+  };
+
+  // delete
+  const deleteProduct = async (productId) => {
+    await axios.delete(`http://localhost:5000/products/${productId}`);
+    getProducts();
+  };
+
   return (
     <div>
       <h1 className="title has-text-danger">Product</h1>
       <h2 className="subtitle has-text-grey">List of Product</h2>
+
+      <Link to="/products/add" className="button is-primary mb-2">
+        Add New
+      </Link>
+
       <table className="table is-striped is-fullwidth">
         <thead>
           <tr>
@@ -16,13 +42,29 @@ const ProductList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {products.map((product, index) => (
+            <tr key={product.uuid}>
+              <td>{index + 1}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.user.name}</td>
+              {/* <td>{product.user ? product.user.name : "Unknown"}</td> */}
+              <td>
+                <Link
+                  to={`/products/edit/${product.uuid}`}
+                  className="button is-small is-info"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => deleteProduct(product.uuid)}
+                  className="button is-small is-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
