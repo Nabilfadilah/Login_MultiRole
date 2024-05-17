1b81df4be689bbe2c572181b5cf4b61006087c74
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-17 16:59:47 +0700

    membuat fungsi logout pada sidebar dan memberikan kontrol agar karyawan tidak bisa mengakses hal users

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,36 +1,62 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { IoPerson, IoPricetag, IoHome, IoLogOut } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { LogOut, reset } from "../Features/authSlice";
 
 const Sidebar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // select usernya dari reducer
+  const { user } = useSelector((state) => state.auth);
+
+  // logout
+  const logout = () => {
+    dispatch(LogOut());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <div>
       <aside className="menu pl-2 has-shadow">
         <p className="menu-label pt-3">General</p>
         <ul className="menu-list">
+          {/* dashboard */}
           <li>
             <NavLink to={"/dashboard"}>
               <IoHome /> Dashboard
             </NavLink>
           </li>
+
+          {/* product */}
           <li>
             <NavLink to={"/products"}>
               <IoPricetag /> Product
             </NavLink>
           </li>
         </ul>
-        <p className="menu-label">Admin</p>
-        <ul className="menu-list">
-          <li>
-            <NavLink to={"/users"}>
-              <IoPerson /> Users
-            </NavLink>
-          </li>
-        </ul>
+
+        {/* users */}
+        {user && user.role === "admin" && (
+          <div>
+            <p className="menu-label">Admin</p>
+            <ul className="menu-list">
+              <li>
+                <NavLink to={"/users"}>
+                  <IoPerson /> Users
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {/* logout */}
         <p className="menu-label">Settings</p>
         <ul className="menu-list">
           <li>
-            <button className="button is-white">
+            <button onClick={logout} className="button is-white">
               <IoLogOut /> logout
             </button>
           </li>
