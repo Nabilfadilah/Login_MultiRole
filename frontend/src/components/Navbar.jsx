615ddf568a2203cae2d0959a0137c8eae27a975c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-17 16:58:33 +0700

    membuat fungsi logout pada navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { LogOut, reset } from "../Features/authSlice";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // select usernya dari reducer
+  const { user } = useSelector((state) => state.auth);
+
+  // logout
+  const logout = () => {
+    dispatch(LogOut());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <div>
       <nav
@@ -37,7 +52,9 @@ const Navbar = () => {
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <button className="button is-light">Log out</button>
+                <button onClick={logout} className="button is-light">
+                  Log out
+                </button>
               </div>
             </div>
           </div>
