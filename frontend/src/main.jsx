1ba2eac6f8f373b0d0081edf31bd95a11984854a
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-17 16:54:13 +0700

    menambahkan store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 import "bulma/css/bulma.css";
 import axios from "axios";
 
@@ -12,7 +13,7 @@ const root = createRoot(container);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
