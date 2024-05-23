4480a12a1ced7ace0983aff45938deed5e9dc84c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-23 14:57:48 +0700

    Membuat form add product

--- frontend/src/components/FormAddProduct.jsx
+++ frontend/src/components/FormAddProduct.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const FormAddProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  // submit
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/products", {
+        name: name,
+        price: price,
+      });
+      navigate("/products");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
   return (
     <div>
       <h1 className="title has-text-danger">Product</h1>
@@ -8,7 +31,8 @@ const FormAddProduct = () => {
       <div className="card is-shadowless">
         <div className="card-content">
           <div className="content">
-            <form>
+            <form onSubmit={saveProduct}>
+              <p className="has-text-center">{msg}</p>
               {/* Name */}
               <div className="field">
                 <label className="label">Name</label>
@@ -16,22 +40,32 @@ const FormAddProduct = () => {
                   <input
                     type="text"
                     className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     placeholder="Product Name"
                   />
                 </div>
               </div>
 
-              {/* Email */}
+              {/* Price */}
               <div className="field">
                 <label className="label">Price</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="Price" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    placeholder="Price"
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <div className="control">
-                  <button className="button is-success">Save</button>
+                  <button className="button is-success" type="submit">
+                    Save
+                  </button>
                 </div>
               </div>
             </form>
