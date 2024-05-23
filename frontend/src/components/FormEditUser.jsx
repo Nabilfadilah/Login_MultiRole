0c72ed6192ac47af56fd1f5b8b58b7636873e143
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-23 14:58:24 +0700

    Membuat form edit user

--- frontend/src/components/FormEditUser.jsx
+++ frontend/src/components/FormEditUser.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const FormEditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    // method
+    const getUserById = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/users/${id}`);
+        console.log(response.data);
+        setName(response.data.name);
+        setEmail(response.data.email);
+        // setPassword(response.data.password);
+        // setConfPassword(response.data.confPassword);
+        setRole(response.data.role);
+      } catch (error) {
+        if (error.response) {
+          setMsg(error.response.data.msg);
+        }
+      }
+    };
+    getUserById();
+  }, [id]);
+
+  // submit
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:5000/users/${id}`, {
+        name: name,
+        email: email,
+        password: password,
+        confPassword: confPassword,
+        role: role,
+      });
+      navigate("/users");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
   return (
     <div>
       <h1 className="title has-text-danger">Users</h1>
@@ -8,12 +58,19 @@ const FormEditUser = () => {
       <div className="card is-shadowless">
         <div className="card-content">
           <div className="content">
-            <form>
+            <form onSubmit={updateUser}>
+              <p className="has-text-center">{msg}</p>
               {/* Name */}
               <div className="field">
                 <label className="label">Name</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="Name" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
+                  />
                 </div>
               </div>
 
@@ -21,7 +78,13 @@ const FormEditUser = () => {
               <div className="field">
                 <label className="label">Email</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="Email" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                  />
                 </div>
               </div>
 
@@ -32,6 +95,9 @@ const FormEditUser = () => {
                   <input
                     type="password"
                     className="input"
+                    // disabled
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     placeholder="******"
                   />
                 </div>
@@ -44,18 +110,25 @@ const FormEditUser = () => {
                   <input
                     type="password"
                     className="input"
+                    // disabled
+                    value={confPassword}
+                    onChange={(e) => setConfPassword(e.target.value)}
                     placeholder="******"
                   />
                 </div>
               </div>
 
+              {/* Role */}
               <div className="field">
                 <label className="label">Role</label>
                 <div className="control">
                   <div className="select is-fullwidth">
-                    <select>
+                    <select
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                    >
                       <option value="admin">Admin</option>
-                      <option value="admin">Karyawan</option>
+                      <option value="karyawan">Karyawan</option>
                     </select>
                   </div>
                 </div>
@@ -63,7 +136,10 @@ const FormEditUser = () => {
 
               <div className="field">
                 <div className="control">
-                  <button className="button is-success">Update</button>
+                  <button className="button is-success" type="submit">
+                    Update
+                  </button>
+                  {msg && <p>{msg}</p>}
                 </div>
               </div>
             </form>
